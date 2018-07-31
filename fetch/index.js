03e10b421ec4966daf27dc65bc391df9b42b68f0
李杰 <lj706517903@163.com>
2018-07-31 10:44:12 +0800

    调整fetch

--- fetch/index.js
+++ fetch/index.js
@@ -1,21 +1,10 @@
-// import { API_URL } from "./APP_ROOT_NETWORK_CONFIG";
-// import { errorHandler, addNetInfoListener } from "./errorHandler";
 import {
     Platform,
     Alert,
     InteractionManager,
     Linking,
 } from "react-native";
-// import {
-//     AppName,
-//     AppPlatform,
-//     errorCollectApi,
-//     env,
-//     developer,
-// } from "./APP_ROOT_CONFIG";
-
-
-import {libraryConfig} from "../libraryConfig"
+import { libraryConfig } from "../libraryConfig"
 
 
 
@@ -23,7 +12,7 @@ export default class FetchDataModule {
     /*
      *  请求入口
     */
-    static fetch({apiName, params}) {
+    static fetch({ apiName, params, headers }) {
         const {
             API_URL,
             getLoginFunc,
@@ -31,27 +20,22 @@ export default class FetchDataModule {
         } = libraryConfig
         const login = getLoginFunc()
         if (apiName) {
-            if (API_URL[apiName].needLogin) {
-                if (login) {
-                    return this.fetchData({apiName, params});
-                } else {
-                    return new Promise(() => {
-                        pushLoginFunc()
-                    })
-                }
-            } else {
-                return this.fetchData({apiName, params})
+            if (API_URL[apiName].needLogin && !login) {
+                return new Promise(() => {
+                    pushLoginFunc()
+                })
             }
+            return this.fetchData({ apiName, params, headers })
         } else {
-            Alert.alert("FetchDataModule模块调用异常，请检查传递参数")
-            return new Promise(()=>{})
+            Alert.alert("request模块调用异常，请检查传递参数")
+            return new Promise(() => { })
         }
     }
 
     /*
      *  处理请求的接口
     */
-    static fetchData({apiName, params}) {
+    static fetchData({ apiName, params, headers }) {
         const {
             API_URL,
             showLoading,
@@ -60,9 +44,9 @@ export default class FetchDataModule {
             showLoading()
         }
         if (API_URL[apiName].method == "GET") {
-            return this.get({apiName, params});
+            return this.get({ apiName, params, headers });
         } else if (API_URL[apiName].method == "POST") {
-            return this.post({apiName, params});
+            return this.post({ apiName, params, headers });
         } else {
             Alert.alert("接口预定义信息错误", `接口名:${apiName}${"\b"}错误类型:请求方式异常`, [
                 {
@@ -94,7 +78,7 @@ export default class FetchDataModule {
     /*
      *  GET请求
     */
-    static get({apiName, params}) {
+    static get({ apiName, params, headers }) {
         const {
             API_URL,
             getHeadersFunc,
@@ -104,23 +88,23 @@ export default class FetchDataModule {
             fetchUrl,
             mockFetchUrl,
         } = API_URL[apiName]
-        return fetch(mock?mockFetchUrl:fetchUrl + "?" + toQueryString(params), {
+        return fetch(mock ? mockFetchUrl : fetchUrl + "?" + toQueryString(params), {
             method: "GET",
-            headers: Object.assign({},mock?{}:getHeadersFunc(),{"Content-Type": "application/x-www-form-urlencoded"}),
+            headers: Object.assign({}, mock ? {} : getHeadersFunc(), { "Content-Type": "application/x-www-form-urlencoded" }, headers),
         })
-        .then(res => {
-            return this.HandleRequestResults({
-                res,
-                apiName,
-                params
+            .then(res => {
+                return this.HandleRequestResults({
+                    res,
+                    apiName,
+                    params
+                })
             })
-        })
     }
 
     /*
      *  POST请求
     */
-    static post({apiName, params}) {
+    static post({ apiName, params, headers }) {
         const {
             API_URL,
             getHeadersFunc,
@@ -130,18 +114,20 @@ export default class FetchDataModule {
             fetchUrl,
             mockFetchUrl,
         } = API_URL[apiName]
-        return fetch(mock?mockFetchUrl:fetchUrl, {
+        return fetch(mock ? mockFetchUrl : fetchUrl, {
             method: "POST",
-            headers: Object.assign({},mock?{}:getHeadersFunc(),{"Content-Type": "application/json"}),
-            body: JSON.stringify(params)
+            // headers: Object.assign({}, mock ? {} : getHeadersFunc(), { "Content-Type": "application/json" }, headers),
+            // body: JSON.stringify(params),
+            headers: Object.assign({}, mock ? {} : getHeadersFunc(), { "Content-Type": "application/x-www-form-urlencoded" }, headers),
+            body: toQueryString(params)
         })
-        .then(res => {
-            return this.HandleRequestResults({
-                res,
-                apiName,
-                params
+            .then(res => {
+                return this.HandleRequestResults({
+                    res,
+                    apiName,
+                    params
+                })
             })
-        })
 
     }
 
@@ -150,7 +136,7 @@ export default class FetchDataModule {
      *  res.headers.map['content-type'][0]                      非debug
      *  res._bodyBlob.type                                      debug
     */
-    static HandleRequestResults({res, apiName, params}) {
+    static HandleRequestResults({ res, apiName, params }) {
 
         const {
             API_URL,
@@ -168,43 +154,42 @@ export default class FetchDataModule {
             hideLoading()
         }
 
-        if (res.headers.map["content-type"][0] != `application/json; charset=utf-8`) {
-            if(env.showNetWorkErrorInfo){
+        if (res.headers.map["content-type"][0] != `application/json;charset=UTF-8`) {
+            if (env.showNetWorkErrorInfo) {
                 res.text()
-                .then(err => {
-                    setTimeout(()=>{
-                        Alert.alert(
-                            "接口请求错误", `接口名:${API_URL[apiName].apiUrl}`,
-                            [
-                                {
-                                    text: "上报接口异常",
-                                    onPress: () => {
-                                        this.ErrorApiFetch({
-                                            apiName,
-                                            errmsg: err,
-                                            params
-                                        })
-                                    }
-                                },
-                                { text: "查看报错信息", onPress: () => console.warn(err) },
-                                { text: "确定", onPress: () => {} }
-                            ]
-                        );
-                    },API_URL[apiName].showLoading?1000:1)
-                });
+                    .then(err => {
+                        setTimeout(() => {
+                            Alert.alert(
+                                "接口请求错误", `接口名:${API_URL[apiName].apiUrl}`,
+                                [
+                                    {
+                                        text: "上报接口异常",
+                                        onPress: () => {
+                                            this.ErrorApiFetch({
+                                                apiName,
+                                                errmsg: err,
+                                                params
+                                            })
+                                        }
+                                    },
+                                    { text: "查看报错信息", onPress: () => console.warn(err) },
+                                    { text: "确定", onPress: () => { } }
+                                ]
+                            );
+                        }, API_URL[apiName].showLoading ? 1000 : 1)
+                    });
             }
-            if(env.defaultUploadNetWorkErrorInfo){
+            if (env.defaultUploadNetWorkErrorInfo) {
                 ToastError("捕获到服务器返回数据类型异常，正在自动提交错误信息");
                 res.text().then(e => {
-                    this.ErrorApiFetch({apiName, errmsg: e, params})
+                    this.ErrorApiFetch({ apiName, errmsg: e, params })
                 });
             }
-            return new Promise((resolve, reject)=>{reject()})
+            return new Promise((resolve, reject) => { reject() })
         } else {
-            return res
-                .json()
+            return res.json()
                 .then(res => {
-                    return new Promise((resolve,reject) => {
+                    return new Promise((resolve, reject) => {
                         if (res.errcode != -999) {
                             resolve(res);
                         } else {
@@ -227,16 +212,17 @@ export default class FetchDataModule {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
         })
-        .then(res => res.json())
-        .then(data => {
-            return new Promise(resolve=>resolve(data))
-        })
+            .then(res => res.json())
+            .then(data => {
+                return new Promise(resolve => resolve(data))
+            })
     }
 
     /*
      *  请求错误处理
     */
-    static ErrorApiFetch({apiName, errmsg, params}) {
+    static ErrorApiFetch({ apiName, errmsg, params }) {
+        return Promise.resolve('错误处理功能暂时关闭')
         const {
             API_URL,
             APP_ROOT_CONFIG,
@@ -260,7 +246,7 @@ export default class FetchDataModule {
 
         fetch(errorCollectApi, {
             method: "POST",
-            headers: Object.assign({},getHeadersFunc(),{"Content-Type": "application/x-www-form-urlencoded"}),
+            headers: Object.assign({}, getHeadersFunc(), { "Content-Type": "application/x-www-form-urlencoded" }),
             body: toQueryString({
                 project: `${AppName}${AppPlatform}端`,
                 post_author: errorApiDeveloper.name,
@@ -269,59 +255,59 @@ export default class FetchDataModule {
                 api_author: API_URL[apiName].author
             })
         })
-        .then(res => {
-            if (res.headers.map["content-type"][0] != `application/json; charset=utf-8`) {
-                Alert.alert("提交错误的接口都报错了", `肿么办ﾍ(;´Д｀ﾍ)`, [
-                    {
-                        text: "GG",
-                        onPress: () => {ToastWarn('你选择了GG')}
-                    },{
-                        text: "人肉联系开发人员",
-                        onPress: () => {
-                            Alert.alert(
-                                `接口的使用者是 ${errorApiDeveloper.name}`,
-                                '是否要拨打电话联系开发者',
-                                [
-                                    {
-                                        text: '取消',
-                                        onPress: () => {}
-                                    },
-                                    {
-                                        text: '拨打',
-                                        onPress: () => {
-                                            Linking.openURL(`tel:${errorApiDeveloper.phone}`).catch(err => console.warn('拨打电话失败，请检查当前环境'));
-                                        }
-                                    },
-                                ]
-                            )
+            .then(res => {
+                if (res.headers.map["content-type"][0] != `application/json; charset=utf-8`) {
+                    Alert.alert("提交错误的接口都报错了", `肿么办ﾍ(;´Д｀ﾍ)`, [
+                        {
+                            text: "GG",
+                            onPress: () => { ToastWarn('你选择了GG') }
+                        }, {
+                            text: "人肉联系开发人员",
+                            onPress: () => {
+                                Alert.alert(
+                                    `接口的使用者是 ${errorApiDeveloper.name}`,
+                                    '是否要拨打电话联系开发者',
+                                    [
+                                        {
+                                            text: '取消',
+                                            onPress: () => { }
+                                        },
+                                        {
+                                            text: '拨打',
+                                            onPress: () => {
+                                                Linking.openURL(`tel:${errorApiDeveloper.phone}`).catch(err => console.warn('拨打电话失败，请检查当前环境'));
+                                            }
+                                        },
+                                    ]
+                                )
+                            }
+                        }, {
+                            text: "确定",
+                            onPress: () => { }
                         }
-                    },{
-                        text: "确定",
-                        onPress: () => {}
-                    }
-                ]);
-            }else {
-                res.json()
-                .then(e => {
-                    ToastInfo("服务器异常提交成功");
-                })
-            }
-        })
+                    ]);
+                } else {
+                    res.json()
+                        .then(e => {
+                            ToastInfo("服务器异常提交成功");
+                        })
+                }
+            })
     }
 }
 
 
 
 // 获取开发者信息
-const developerVerification = ({developerName,developer,ToastError})=>{
-    if(developerName){
-        if(developer.allDeveloper[developerName]){
+const developerVerification = ({ developerName, developer, ToastError }) => {
+    if (developerName) {
+        if (developer.allDeveloper[developerName]) {
             return developer.allDeveloper[developerName]
-        }else {
+        } else {
             ToastError('检测到定义了错误的接口开发者，请审查APP_ROOT_NETWORK_CONFIG')
             return developer.main
         }
-    }else {
+    } else {
         return developer.main
     }
 }
@@ -329,27 +315,27 @@ const developerVerification = ({developerName,developer,ToastError})=>{
 function toQueryString(obj) {
     return obj
         ? Object.keys(obj)
-              .sort()
-              .map(function(key) {
-                  var val = obj[key];
-                  if (Array.isArray(val)) {
-                      return val
-                          .sort()
-                          .map(function(val2) {
-                              return encodeURIComponent(key) +
-                                  "[]=" +
-                                  encodeURIComponent(val2);
-                          })
-                          .join("&");
-                  }
-                  if (val) {
-                      return encodeURIComponent(key) +
-                          "=" +
-                          encodeURIComponent(val);
-                  } else {
-                      return encodeURIComponent(key) + "=";
-                  }
-              })
-              .join("&")
+            .sort()
+            .map(function (key) {
+                var val = obj[key];
+                if (Array.isArray(val)) {
+                    return val
+                        .sort()
+                        .map(function (val2) {
+                            return encodeURIComponent(key) +
+                                "[]=" +
+                                encodeURIComponent(val2);
+                        })
+                        .join("&");
+                }
+                if (val) {
+                    return encodeURIComponent(key) +
+                        "=" +
+                        encodeURIComponent(val);
+                } else {
+                    return encodeURIComponent(key) + "=";
+                }
+            })
+            .join("&")
         : "";
 }
